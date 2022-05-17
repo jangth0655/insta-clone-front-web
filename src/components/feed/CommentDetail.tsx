3fddb5a3e277ceee59eb066d30913ecb0ac07782
JANGTAEHEE <jangth0655@gmail.com>
2022-05-17 09:20:07 +0900

    parsing hashtags

--- src/components/feed/CommentDetail.tsx
+++ src/components/feed/CommentDetail.tsx
@@ -1,3 +1,5 @@
+import React from "react";
+import { Link } from "react-router-dom";
 import styled from "styled-components";
 import { FatText } from "../shared";
 
@@ -7,6 +9,14 @@ const CommentContainer = styled.div`
 
 const CommentCaption = styled.span`
   margin-left: 10px;
+  a {
+    background-color: inherit;
+    color: ${(props) => props.theme.accent};
+    cursor: pointer;
+    &:hover {
+      text-decoration: underline;
+    }
+  }
 `;
 
 interface CommentDetailProps {
@@ -21,7 +31,17 @@ const CommentDetail = ({ user, payload }: CommentDetailProps) => {
   return (
     <CommentContainer>
       <FatText>{user?.username}</FatText>
-      <CommentCaption>{payload}</CommentCaption>
+      <CommentCaption>
+        {payload?.split(" ").map((word, i) =>
+          /#[\w]+/.test(word) ? (
+            <React.Fragment key={i}>
+              <Link to={`/hashtags/${word}`}>{word}</Link>{" "}
+            </React.Fragment>
+          ) : (
+            <React.Fragment key={i}>{word} </React.Fragment>
+          )
+        )}
+      </CommentCaption>
     </CommentContainer>
   );
 };
