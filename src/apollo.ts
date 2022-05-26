e19dc069a2de8f74e828febd1efe574ee7bd7d1a
JANGTAEHEE <jangth0655@gmail.com>
2022-05-26 17:46:33 +0900

    netlify

--- src/apollo.ts
+++ src/apollo.ts
@@ -37,7 +37,10 @@ export const disabledDarkMode = () => {
 };
 
 const httpLink = createHttpLink({
-  uri: `http://localhost:4000/graphql`,
+  uri:
+    process.env.NODE_ENV === "production"
+      ? "wss://instaclone-backend-taehee.herokuapp.com/graphql"
+      : "`ws://localhost:4000/graphql`",
 });
 
 const authLink = setContext((_, { headers }) => {
